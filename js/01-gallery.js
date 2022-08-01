f1428b83b31b627c96bda0f08c43408b9c27ed72
Evolokhin <evolokhin@rightandabove.com>
2022-08-01 20:17:27 +0300

    -task 1 image gallery completed with keyboard esc button listener

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const pictures = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", pictures);
+
+gallery.addEventListener("click", openPicture);
+
+function openPicture(event) {
+  event.preventDefault();
+  const imgUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">
+`);
+
+  const imageClose = (event) => {
+    if (event.key === "Escape")
+      instance.close(() => {
+        document.removeEventListener("keydown", imageClose);
+      });
+  };
+
+  document.addEventListener("keydown", imageClose);
+
+  instance.show();
+}
